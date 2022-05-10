2cfbe2fa03e96440b616cf2e79543eb7f1536689
brokenfingers <buvotrys@gmail.com>
2022-05-10 21:04:34 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Props</h1>
-                <Car pr='5000' maker="Opel" color='blue' size='15' font='Courier' clr="yellow" markSize="40"></Car>
+                <Car pr='3500' maker="Opel" color='blue' size='15' font='Courier' clr="yellow" markSize="40"></Car>
                 <Car pr='9000' maker="Volvo" color='yellow' size='22' font='Arial' clr="red" markSize="60"></Car>
-                <Car pr='8000' maker="MB" color='silver' size='24' font='Geneva' clr="green" markSize="20"></Car>
+                <Car pr='3000' maker="MB" color='silver' size='24' font='Geneva' clr="green" markSize="20"></Car>
             </header>
         </div>
     )
--- src/Components/002/CarPrice.jsx
+++ src/Components/002/CarPrice.jsx
@@ -1,7 +1,7 @@
 function CarPrice(props) {
     return (
         <>
-            {props.price > 4000 ? <h3>props.price+'$'</h3> : <h4>props.price+'$'</h4>}
+            {+props.price > 4000 ? <h3>{props.price+'$'}</h3> : <h4>{props.price+'$'}</h4>}
         </>
     )
 }
