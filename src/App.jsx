f950615224cd3cf993b73b2350555659ec239455
brokenfingers <buvotrys@gmail.com>
2022-05-14 13:31:49 +0300

    add square

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import {useState} from 'react';
 function App() {
 
     const [show, setShow] = useState(true)
+    const [show1, setShow1] = useState(true)
+
     const [color, setColor] = useState('blue')
     const [numb, setNumber] = useState(1)
-     
-    const doShow = () => {
-       setShow(s => !s);
+    
+    const [sqaureArray, setAddSq] = useState([])
+    
+    const doShow = (arg) => {
+        arg === 1 ? setShow(s => !s) :setShow1(s => !s)
     }
 
     // const setRedFnct = () => {
@@ -19,29 +23,30 @@ function App() {
     //     setColor('yellow')
     // }
 
-    const setColorfunct = color => {
-        setColor(color)
-    }
-
+    const setColorfunct = color =>  setColor(color)
     // const addNumber = () => {
     //     setNumber(s => ++s)
         
     // }
     //  const subtrNumber = () => {
     //     setNumber(s => --s)
-        
     // }
 
-    const makeCalc = operator => {
-        setNumber(s => eval(`${s}${operator}1`))
+    const addSquare = () => {
+
+            setAddSq(s => [...s, 1])
     }
 
+    const makeCalc = operator => setNumber(s => eval(`${s}${operator}1`))
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Hook {numb}</h1>
-                    {show ? <div className="square" style={{backgroundColor:color}}></div> : null}
-                <button onClick={doShow}>show/hide</button>
+                    {show ? <div className="square" style={{backgroundColor:color}}>1</div> : null}
+                    {show1 ? <div className="square" style={{backgroundColor:color}}>2</div> : null}
+                <button onClick={() => doShow(1)}>show/hide 1</button>
+                <button onClick={() => doShow(2)}>show/hide 2</button>
+
                 <button onClick={() => setColorfunct('red')}>red</button>
                 <button onClick={() => setColorfunct('yellow')}>red</button>
                 <button onClick={() => setColorfunct('pink')}>red</button>
@@ -49,6 +54,14 @@ function App() {
                 <button onClick={() => makeCalc('+')}>add number</button>
                 <button onClick={() => makeCalc('-')}>subtr number</button>
                 
+                <button onClick={addSquare}>add square</button>
+                
+                    <div className='square-wrapper'>
+                        {
+                         sqaureArray.map(itm => <div className="square" style={{backgroundColor:color}}>2</div>)
+                        }
+
+                    </div>
                 
 
             </header>
