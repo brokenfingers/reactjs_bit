1fa5247ecaedb40551e46e759b755747d344c1f9
brokenfingers <buvotrys@gmail.com>
2022-05-18 00:11:12 +0300

    set ear color

--- src/Components/007/Apskritimas.jsx
+++ src/Components/007/Apskritimas.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import randColor from "../../Functions/randColor";
+
 function Apskritimas({l, visible, widt}) {
 
+    const [cl, setColor] = useState('');
+
+    const onPressEarr = () => {
+        setColor('#'+randColor())
+    }
+
     if(visible) {
-       return <div className='apskritimas' style={{left: l, bottom: '88%', borderWidth: widt}}></div> 
+       return <div onClick={onPressEarr} className='apskritimas' style={{left: l, bottom: '88%', borderWidth: widt, backgroundColor: cl}}></div> 
     }
     return null;
 }
