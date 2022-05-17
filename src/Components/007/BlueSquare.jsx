15faac7866af7b698146be799f900033bb8ea67a
brokenfingers <buvotrys@gmail.com>
2022-05-17 23:06:17 +0300

    toggle

--- src/Components/007/BlueSquare.jsx
+++ src/Components/007/BlueSquare.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import randColor from '../../Functions/randColor'
 
 function BlueSquare() {
 
@@ -9,11 +10,23 @@ function BlueSquare() {
         colorFunct('red');
     }
 
+    const makeRandom = () => {
+
+        colorFunct('#'+randColor());
+    }
+
+    const toggleBlueRed = () => {
+        colorFunct( clr === 'blue' ? 'red' : 'blue')
+    }
+
     return(
         <>
             <div className='kvadratas-big' style={{backgroundColor: clr}}>
 
-                <button onClick={makeRed} >change color</button>
+                <button onClick={makeRed} >change to red</button>
+                <button onClick={makeRandom} >ramdom backgroudn</button>
+                <button onClick={toggleBlueRed} >toggleRedBlue</button>
+
             </div>
             
         </>
