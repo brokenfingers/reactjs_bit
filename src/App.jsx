b561f5ee1cdb3a11128b4bb23d8fee0c3ca4a5f0
brokenfingers <buvotrys@gmail.com>
2022-05-19 00:05:28 +0300

    increase

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,26 @@ import { useState} from 'react'
 function App() {
 
     const [bgColor, setColor] = useState('pink')
-    
+    const [numbr, setIncrease] = useState(1)
+    const [redBg, setRedBg] = useState(null)
+
     const onSetColor = () => {
         setColor(i => i === 'pink' ? 'green' : 'pink');
+    }
+
+    const onSetIncrease = () => {
+        setIncrease(i => redBg === 'red' ? --i : ++i)
+    }
+
+    const onSetRed = () => {
+        setRedBg(i => i===null ? 'red' : null)
     }
 
     return (
         <div className="App">
             <header className="App-header">
-                <Buttnon virvute={onSetColor}></Buttnon>
+                <h1>Uplifting <span style={{color:bgColor, backgroundColor: redBg}}>{numbr}</span></h1>
+                <Buttnon virvute={onSetColor} virvute2={onSetIncrease} onSetRed={onSetRed}></Buttnon>
                 <Green bgc={bgColor}></Green>
             </header>
 
--- src/Components/008/Buttnon.jsx
+++ src/Components/008/Buttnon.jsx
@@ -1,8 +1,10 @@
-function Buttnon({virvute}) {
+function Buttnon({virvute, virvute2, onSetRed}) {
 
     return (
         <>
             <button onClick={virvute}>Change color</button>
+            <button onClick={virvute2}>increase number</button>
+            <button onClick={onSetRed}>setRed</button>
         </>
     )
 }
