95f2ee9884bd971816ded68815bd41a016987ae6
brokenfingers <buvotrys@gmail.com>
2022-05-20 20:41:40 +0300

    set color

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const [anArray, setArray] = useState([]);
 
 const onButton = (e) => {
 
-    setArray(it=> [...it ?? '', text])
+    setArray(it=> text !=='' ? [...it ?? '', text] : it)
     setText('');
 }
 
@@ -27,6 +27,22 @@ const handleText = (e) => {
 }
 
 
+const [clrArr, setClrArr] = useState([]);
+
+const [color, setColor] = useState('')
+
+const onColorChange = (e) => {
+
+    setColor(e.target.value )
+}
+
+const addColor = () => {
+    setClrArr(i => color !== '' ? [...i || '#ffffff', color] : i)
+}
+
+const clearColor = () => {
+    setClrArr([])
+}
 
     return (
         <div className="App">
@@ -44,11 +60,22 @@ const handleText = (e) => {
                         <button className='btn btn-primary' onClick={onButton}>add</button>
                         <button className='btn btn-primary' onClick={onClearList}>clear</button>
                     </div>
+
+                    <div className="form-group">
+                        <input type="color" className="form-control"  aria-describedby="emailHelp" value={color} onChange={onColorChange}/>
+                        <button className='btn btn-primary' onClick={addColor}>add</button>
+                        <button className='btn btn-primary' onClick={clearColor}>clear</button>
+                    </div>
                     
 
                     <ul className="list-group">
                         {
                             anArray.map((ir, i )=> <li className="list-group-item" key={i}>{ir}</li>)
+                        }
+                    </ul>
+                    <ul className="list-group">
+                        {
+                            clrArr.map((ir, i )=> <li style={{backgroundColor:ir}}className="list-group-item" key={i}>{ir}</li>)
                         }
                     </ul>
                 </div>
