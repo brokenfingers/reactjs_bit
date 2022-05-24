0cc7d7f9732a980b608b59f56ac00c414a138133
brokenfingers <buvotrys@gmail.com>
2022-05-24 21:14:03 +0300

    radio input

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { useState } from 'react';
 function App() {
 
     const [checkbox, setCheckbox] = useState({a:true, b:false, c: false, d: true});
+    const [radio, setRadio] = useState('option1')
+    const [list, setList] = useState([])
+    const [radioList, setRadioList] = useState([])
     
     const  handleCheckbox = (checked) => {
         setCheckbox(i => ({...i, [checked]: !i[checked]}))
     }
-
-    const [list, setList] = useState([])
 
     const handleList = () => {
         setList(i => [...i, (checkbox.a ? 'A' : '')+(checkbox.b ? 'B' : '')+(checkbox.c ? 'C' : '')+(checkbox.d ? 'D' : '')])
@@ -19,31 +20,54 @@ function App() {
         setList(i => [])
     }
 
+    const handleRadio = (e) => { setRadio(e.target.value) }
+    const handleRadioList = () => {
+        setRadioList(i=>[...i, radio])
+    }
+
+    const handleRadioClear = () => { setRadioList( i => []) }
     return (
         <div className="App">
             <header className="App-header">
                 <div className="card">
-                    <p className="title">Lentele 1</p>
+                    <p className="title">Checkbox</p>
                     <label for="vehicle1"> Check A</label>
-                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('a')} value={checkbox.a} checked={checkbox.a}></input>
+                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('a')} value={checkbox.a} checked={checkbox.a}/>
                     <label for="vehicle1"> Check B</label>
-                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('b')} value={checkbox.b} checked={checkbox.b}></input>
+                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('b')} value={checkbox.b} checked={checkbox.b}/>
                     <label for="vehicle1"> Check C</label>
-                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('c')} value={checkbox.c} checked={checkbox.c}></input>
+                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('c')} value={checkbox.c} checked={checkbox.c}/>
                     <label for="vehicle1"> Check D</label>
-                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('d')} value={checkbox.d} checked={checkbox.d}></input>
+                   <input type="checkbox" name="vehicle1" onChange={()=>handleCheckbox('d')} value={checkbox.d} checked={checkbox.d}/>
                    <button onClick={handleList}> add to list</button>
                 </div>
                 <div className="card">
                     <ul>
-
-
                     {
                         list.map((itm, i) => <li key={i}>{itm}</li>)
                      }  
-                    
                     </ul>
                     <button onClick={handleClear}>Clear list</button>
+                </div>
+                <div className='card'>
+                    <p className="title">Radio</p>
+                     <input type="radio" name="fav" value="option1" onChange={handleRadio} checked={radio === 'option1'}/>
+                    <label>option 1</label>
+                     <input type="radio" name="fav" value="option2"  onChange={handleRadio} checked={radio === 'option2'}/>
+                    <label>option 2</label>
+                     <input type="radio" name="fav" value="option3"  onChange={handleRadio} checked={radio === 'option3'}/>
+                    <label>option 3</label>
+                     <input type="radio" name="fav" value="option4" onChange={handleRadio} checked={radio === 'option4'}/>
+                    <label>option 4</label>
+                    <button onClick={handleRadioList}>radio to list</button>
+                </div>
+                <div className="card">
+                    <ul>
+                    {
+                        radioList.map((itm, i) => <li key={i}>{itm}</li>)
+                     }  
+                    </ul>
+                    <button onClick={handleRadioClear}>Clear list</button>
                 </div>
             </header>
         </div>
