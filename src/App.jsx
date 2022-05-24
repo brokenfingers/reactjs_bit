d9cf120ff2b4d0dd480923b32549afed8951ec6d
brokenfingers <buvotrys@gmail.com>
2022-05-24 23:16:01 +0300

    add squares local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 function App() {
 
     const [number, setNumber] = useState(null)
-
+    const [squareArr, setSquareArr] = useState([])
 
     const handleButton = () => {
             setNumber(i => {
@@ -18,12 +18,41 @@ function App() {
         setNumber(i => +localStorage.getItem('number') ?? 1)
     }, [])
 
+    const handleAddSquare = () => {
+        setSquareArr(i => {
+             localStorage.setItem('square', JSON.stringify([...squareArr, 1]))
+            return [...i, 1]
+        }
+            )
+        
+    }
+
+    useEffect(()=>{
+        setSquareArr(JSON.parse(localStorage.getItem('square') ?? '[]')  )
+    }, [])
 
     return (
         <div className="App">
             <header className="App-header">
-            <p>{number}</p>
-            <button onClick={handleButton}>add</button>
+                <div className="card">
+                    <p className="title">add one</p>
+                    
+                    <button onClick={handleButton}>add</button>
+                </div>
+                <div className='card'>
+                    <p>{number}</p>
+                </div>
+
+                <div className="card">
+                    <p className="title">squares</p>
+                    <button onClick={handleAddSquare}>Add square</button>
+                </div>
+                <div className='card'>
+                    {
+                        squareArr.map((_, i) => <div className='square' key={i}></div>)
+                    }
+                </div>
+            
             </header>
         </div>
     )
