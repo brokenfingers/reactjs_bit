6462d58ba5ca81deeadd1361ed3cdfcd03ae92f4
brokenfingers <buvotrys@gmail.com>
2022-05-26 22:55:57 +0300

    repeat useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import { useEffect, useState } from 'react';
 
 
 function App() {
+    const [message, setMessage] = useState('')
     const labas = () => {
         console.log('Labas')
+    }
+
+    const handleExit = () => {
+        setMessage('ate')
     }
 
     return (
         <div className="App">
             <header className="App-header">
-                <button onClick={labas}></button>
+                <h2>{message}</h2>
+                <button onClick={labas}>hi</button>
+                <button onClick={handleExit }>say bye</button>
             </header>
         </div>
     )
