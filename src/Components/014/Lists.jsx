10fed86a05b70d5590048dad4dfec8aacaf5935c
brokenfingers <buvotrys@gmail.com>
2022-05-27 00:25:42 +0300

    show hide

--- src/Components/014/Lists.jsx
+++ src/Components/014/Lists.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 function List() {
+    const [display, setDisplay] = useState(false)
     const sizes = ['XS', 'S', 'XM', 'M', 'L', 'XL', 'XXL']
+
+    const toggleShow = () => setDisplay(i => !i)
     return (
         <>
+        
         {
-            sizes.map((itm, i) => <div key={i}>{itm}</div>)
+            sizes.filter(i => i[0]==="M").map((itm, i) => i%2 ? <div style={{backgroundColor:  'yellow' }} key={i}><i>{itm}</i></div> : 
+            <div style={{backgroundColor:  'red' }} key={i}><u>{itm}</u></div>)
         }
+         {
+            sizes.sort((a, b)=> b.length - a.length).map((itm, i) => i%2 ? <div style={{backgroundColor:  'yellow' }} key={i}><i>{itm}</i></div> : 
+            <div style={{backgroundColor:  'red' }} key={i}><u>{itm}</u></div>)
+        }
+         {
+            sizes.filter(itm => display).map((itm, i) => i%2 ? <div style={{backgroundColor:  'yellow' }} key={i}><i>{itm}</i></div> : 
+            <div style={{backgroundColor:  'red' }} key={i}><u>{itm}</u></div>)
+        }
+        <button onClick={toggleShow}>show/ hide</button>
         </>
+        
     )
 }
 
