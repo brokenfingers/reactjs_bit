603643373eedd3f11b4f0d8a9be69fc70892a778
brokenfingers <buvotrys@gmail.com>
2022-05-26 23:00:04 +0300

    repeat toggle ate

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,21 @@ function App() {
     const handleExit = () => {
         setMessage('ate')
     }
+    const handleExitClear = () => {
+        setMessage('')
+    }
+    const handleToggleAte = () => {
+        setMessage(message === '' ? 'ate' : '')
+    }
 
     return (
         <div className="App">
             <header className="App-header">
                 <h2>{message}</h2>
                 <button onClick={labas}>hi</button>
-                <button onClick={handleExit }>say bye</button>
+                <button onClick={handleExit}>say bye</button>
+                <button onClick={handleExitClear}>not ate</button>
+                <button onClick={handleToggleAte}>toggle ate</button>
             </header>
         </div>
     )
