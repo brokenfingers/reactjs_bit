d66aed131f6aae5abe7c940cdc846c5c9d28b84a
brokenfingers <buvotrys@gmail.com>
2022-06-09 00:07:30 +0300

    reload

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   const [fetchedBooks, dispatchBooks] = useReducer(booksReducer, null )
 
     useEffect(() => {
-        if(!fetchedBooks) {
+        if(fetchedBooks) return
         axios.get('https://in3.dev/knygos/')
       .then(function (response) {
           dispatchBooks({type: 'uzkrauta', payload: response.data})
 
         })
-      }}
+      }
      , [fetchedBooks]);
 
     return (
@@ -23,8 +23,12 @@ function App() {
             <h1>Books</h1>
             <div className='card'>
               <button onClick={()=>dispatchBooks({type: 'sort_abc'})}>Rusiuoti pagal abc</button>
+              <button onClick={()=>dispatchBooks({type: 'sort_default'})}>Default sort</button>
+              <button onClick={()=>dispatchBooks({type: 'price_above_13'})}>Filter price above 13</button>
+              <button onClick={()=>dispatchBooks({type: 'show_all'})}>Show all</button>
+              <button onClick={()=>dispatchBooks({type: 'get_new'})}>Get new list from server</button>
               {
-                fetchedBooks ? fetchedBooks.map((itm) => <div key={itm.id}>{itm.title}</div>) : <h2>Loading...</h2>
+                fetchedBooks ? fetchedBooks.map((itm) => itm.show && <div key={itm.id}>{itm.title}<span>{itm.price}</span></div>) : <h2>Loading...</h2>
               }
             </div>
           </header>
--- src/Reducers/booksReducer.js
+++ src/Reducers/booksReducer.js
@@ -2,14 +2,32 @@ function booksReducer(state, action) {
     let newState;
     switch (action.type) {
         case 'uzkrauta':
-            newState = action.payload
+            newState = [...action.payload].map((itm, i) => ({ ...itm, row: i, show: true }))
             break;
         case 'sort_abc':
             console.log('dddd')
             newState = [...state].sort((a, b) => {
                 if (a.title[0] > b.title[0]) return 1
                 if (a.title[0] < b.title[0]) return -1
+                return 0
             })
+
+            break;
+        case 'sort_default':
+
+            newState = [...state].sort((a, b) => a.row - b.row)
+
+            break;
+        case 'price_above_13':
+            newState = state.map(itm => (+itm.price > 13) ? ({ ...itm, show: true }) : ({ ...itm, show: false }))
+
+            break;
+        case 'show_all':
+            newState = state.map(itm => ({ ...itm, show: true }))
+
+            break;
+        case 'get_new':
+            newState = null
 
             break;
         default:
